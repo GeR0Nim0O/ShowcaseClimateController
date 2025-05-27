a09e88da896ae91f831af8741ec6d6b1ed5240c2
GeR0Nim0O <88638730+GeR0Nim0O@users.noreply.github.com>
2025-05-27 10:28:36 +0200

    Auto-commit on save

--- lib/RTC/DS3231rtc/DS3231rtc.h
+++ lib/RTC/DS3231rtc/DS3231rtc.h
@@ -12,12 +12,18 @@ class DS3231rtc : public Device {
 public:
     DS3231rtc(TwoWire* wire, uint8_t i2cChannel, uint8_t tcaPort, float threshold, std::map<String, String> channels, int deviceIndex);
     bool begin() override;
+    bool isConnected() override;
+    void update() override;
     std::map<std::string, float> readData() override; // Return a map of sensor data
+    
+    // Override pure virtual methods from Device
+    std::map<String, String> getChannels() const override { return channels; }
+    float getThreshold(const String& channelKey) const override { return threshold; }
+    
     void setTime(byte second, byte minute, byte hour, byte dayOfWeek, byte dayOfMonth, byte month, byte year);
     bool readTime(byte* second, byte* minute, byte* hour, byte* dayOfWeek, byte* dayOfMonth, byte* month, byte* year);
     void displayTime();
     uint8_t getTcaPort() const { return tcaPort; } // Add getter method
-    std::map<String, String> getChannels() const { return channels; } // Add getter method for channels
     int getDeviceIndex() const { return deviceIndex; } // Add getDeviceIndex function
 
 private:
