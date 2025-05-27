c27e582248266d3306472cbf4f8b049aaf0b27dd
GeR0Nim0O <88638730+GeR0Nim0O@users.noreply.github.com>
2025-05-27 10:29:55 +0200

    Auto-commit on save

--- lib/DAC/DAC_Module/DAC_Module.h
+++ lib/DAC/DAC_Module/DAC_Module.h
@@ -20,15 +20,20 @@
 class DAC_Module : public Device {
 public:
     DAC_Module(uint8_t i2cAddress, uint8_t tcaChannel, const String& deviceName, int deviceIndex);
+    DAC_Module(TwoWire* wire, uint8_t i2cChannel, uint8_t tcaPort, float threshold, std::map<String, String> channels, int deviceIndex);
     
     bool begin() override;
     bool isConnected() override;
     void update() override;
+    std::map<std::string, float> readData() override;
+    
+    // Override pure virtual methods from Device
+    std::map<String, String> getChannels() const override { return channels; }
+    float getThreshold(const String& channelKey) const override { return threshold; }
     
     // DAC control methods
     bool setChannelA(uint16_t value);
     bool setChannelB(uint16_t value);
-    bool setChannelVoltage(uint8_t channel, float voltage);
     bool setBothChannels(uint16_t valueA, uint16_t valueB);
     
     // Climate control specific methods
