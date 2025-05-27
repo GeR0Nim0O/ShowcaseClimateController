0d3b76a18f454e394ef18a4f99237f7613826078
GeR0Nim0O <88638730+GeR0Nim0O@users.noreply.github.com>
2025-05-27 09:58:28 +0200

    Auto-commit on save

--- src/main.cpp
+++ src/main.cpp
@@ -49,13 +49,12 @@ void gitAutoCommitAndPush() {
 
 void initializeDevices() {
     Serial.println("=== Initializing Climate Controller Devices ===");
-    
-    // Create device instances
+      // Create device instances
     gpioExpander = new PCF8574_GPIO(GPIO_EXPANDER_ADDR, GPIO_EXPANDER_CHANNEL, "GPIO_Expander", 0);
     tempHumSensor = new SHT31_Sensor(SHT31_ADDR, SHT31_CHANNEL, "Temperature_Humidity_Sensor", 0);
     display = new Display(DISPLAY_ADDR, DISPLAY_CHANNEL, "OLED_Display", 0);
     dacModule = new DAC_Module(DAC_ADDR, DAC_CHANNEL, "DAC_Module", 0);
-    encoder = new RotaryEncoder(ENCODER_PIN_A, ENCODER_PIN_B, ENCODER_BUTTON_PIN, "Rotary_Encoder", 0);
+    encoder = new RotaryEncoder(ENCODER_ADDR, ENCODER_CHANNEL, "I2C_Encoder", 0);
     
     // Register devices with the device registry
     deviceRegistry.registerDevice(gpioExpander);
