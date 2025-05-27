2e1739d93c4111828ca4492b602c2b2eb0c7bd3f
GeR0Nim0O <88638730+GeR0Nim0O@users.noreply.github.com>
2025-05-27 22:09:32 +0200

    Auto-commit on save

--- lib/RTC/DS3231rtc/DS3231rtc.h
+++ lib/RTC/DS3231rtc/DS3231rtc.h
@@ -2,32 +2,39 @@
 #define DS3231RTC_H
 
 #include "Device.h"
-#include <Arduino.h> // Include Arduino header for byte type
+#include <Wire.h>
 #include <map>
 #include <string>
 
-#define DS3231_ADDRESS 0x68 // Define the I2C address for the DS3231
+#define DS3231_ADDRESS 0x68
 
 class DS3231rtc : public Device {
 public:
-    DS3231rtc(TwoWire* wire, uint8_t i2cChannel, uint8_t tcaPort, float threshold, std::map<String, String> channels, int deviceIndex);
-    bool begin() override;
+    DS3231rtc(TwoWire* wire, uint8_t i2cChannel, uint8_t tcaPort, float threshold, std::map<String, String> channels, int deviceIndex)
+        : Device(i2cChannel, tcaPort, threshold, channels, deviceIndex), wire(wire) {}
+    
+    bool begin() override {
+        wire->begin();
+        return isConnected();
+    }
+    
     bool isConnected() override;
     void update() override;
-    std::map<String, String> readData() override; // Return a map of sensor data    
-    // Override pure virtual methods from Device
-    std::map<String, String> getChannels() const override { return channels; }
-    float getThreshold(const String& channelKey = "") const override { return threshold; }
+    std::map<String, String> readData() override;
+    float getThreshold(const String& channelKey) const override { return threshold; }
     
-    void setTime(byte second, byte minute, byte hour, byte dayOfWeek, byte dayOfMonth, byte month, byte year);
-    bool readTime(byte* second, byte* minute, byte* hour, byte* dayOfWeek, byte* dayOfMonth, byte* month, byte* year);
-    void displayTime();
-
+    bool readTime(unsigned char* second, unsigned char* minute, unsigned char* hour,
+                 unsigned char* dayOfWeek, unsigned char* dayOfMonth, unsigned char* month,
+                 unsigned char* year);
+                 
+    bool setTime(unsigned char second, unsigned char minute, unsigned char hour,
+                unsigned char dayOfWeek, unsigned char dayOfMonth, unsigned char month,
+                unsigned char year);
+    
 private:
     TwoWire* wire;
-    uint8_t _address;
-    byte decToBcd(byte val);
-    byte bcdToDec(byte val);
+    unsigned char decToBcd(unsigned char val);
+    unsigned char bcdToDec(unsigned char val);
 };
 
 #endif // DS3231RTC_H
