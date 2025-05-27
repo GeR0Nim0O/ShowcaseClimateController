e304b6199b960fdfea7373f4c585756f8d4534eb
GeR0Nim0O <88638730+GeR0Nim0O@users.noreply.github.com>
2025-05-27 10:30:55 +0200

    Auto-commit on save

--- lib/DAC/DAC_Module/DAC_Module.h
+++ lib/DAC/DAC_Module/DAC_Module.h
@@ -21,11 +21,10 @@ class DAC_Module : public Device {
 public:
     DAC_Module(uint8_t i2cAddress, uint8_t tcaChannel, const String& deviceName, int deviceIndex);
     DAC_Module(TwoWire* wire, uint8_t i2cChannel, uint8_t tcaPort, float threshold, std::map<String, String> channels, int deviceIndex);
-    
-    bool begin() override;
+      bool begin() override;
     bool isConnected() override;
     void update() override;
-    std::map<std::string, float> readData() override;
+    std::map<String, String> readData() override;
     
     // Override pure virtual methods from Device
     std::map<String, String> getChannels() const override { return channels; }
