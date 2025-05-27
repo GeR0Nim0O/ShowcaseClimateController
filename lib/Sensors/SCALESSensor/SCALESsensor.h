70783ddedfeefb9c960bf4cb86949d17dbd4c3f6
GeR0Nim0O <88638730+GeR0Nim0O@users.noreply.github.com>
2025-05-27 11:18:54 +0200

    Auto-commit on save

--- lib/Sensors/SCALESSensor/SCALESsensor.h
+++ lib/Sensors/SCALESSensor/SCALESsensor.h
@@ -27,8 +27,6 @@ private:
     TwoWire* wire;
     uint8_t _address;
     float _weight;
-    std::map<String, String> channels; // Add channels property
-    int deviceIndex; // Add deviceIndex property
 };
 
 #endif // SCALESSENSOR_H
