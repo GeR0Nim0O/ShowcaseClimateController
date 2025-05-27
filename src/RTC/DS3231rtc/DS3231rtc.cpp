0b0e75e4c121c9692e710e3ef0490ce1c43d9045
GeR0Nim0O <88638730+GeR0Nim0O@users.noreply.github.com>
2025-05-27 22:06:18 +0200

    Auto-commit on save

--- src/RTC/DS3231rtc/DS3231rtc.cpp
+++ src/RTC/DS3231rtc/DS3231rtc.cpp
@@ -31,4 +31,31 @@ bool DS3231rtc::begin() {
     initialized = true;
     return true;
 }
+
+// Stub implementation for setTime
+void DS3231rtc::setTime(unsigned char a, unsigned char b, unsigned char c, unsigned char d, unsigned char e, unsigned char f, unsigned char g) {
+    // TODO: Implement RTC time setting
+}
+
+// Stub implementation for readTime
+void DS3231rtc::readTime(unsigned char* a, unsigned char* b, unsigned char* c, unsigned char* d, unsigned char* e, unsigned char* f, unsigned char* g) {
+    // TODO: Implement RTC time reading
+}
+
+// Stub implementation for isConnected
+bool DS3231rtc::isConnected() {
+    // TODO: Implement RTC connection check
+    return true;
+}
+
+// Stub implementation for update
+void DS3231rtc::update() {
+    // TODO: Implement RTC update
+}
+
+// Stub implementation for readData
+void DS3231rtc::readData() {
+    // TODO: Implement RTC data reading
+}
+
 // ...rest of the file unchanged...
