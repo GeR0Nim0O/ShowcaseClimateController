9ad4989392c7ddae52cf5ea0ffbc13f271393f74
GeR0Nim0O <88638730+GeR0Nim0O@users.noreply.github.com>
2025-05-27 21:08:12 +0200

    Auto-commit on save

--- lib/Sensors/SHT31Sensor/SHT31sensor.h
+++ lib/Sensors/SHT31Sensor/SHT31sensor.h
@@ -16,7 +16,7 @@ public:
     bool isConnected() override;
     void update() override;
     std::map<String, String> readData() override; // Return a map of sensor data
-      // Override pure virtual methods from Device
+reg      // Override pure virtual methods from Device
     float getThreshold(const String& channelKey) const override { return threshold; }
     
     float getTemperature() const;
