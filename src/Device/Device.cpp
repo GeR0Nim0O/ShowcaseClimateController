03e41b4a661917089880cf7ba4c662732ae7bc02
GeR0Nim0O <88638730+GeR0Nim0O@users.noreply.github.com>
2025-05-28 21:23:39 +0200

    Auto-commit on save

--- src/Device/Device.cpp
+++ src/Device/Device.cpp
@@ -2,14 +2,14 @@
 
 #define PCA9548A_ADDRESS 0x70
 
-Device::Device(uint8_t i2cAddress, uint8_t tcaChannel, const String& deviceName, int deviceIndex)
-    : i2cAddress(i2cAddress), tcaChannel(tcaChannel), deviceName(deviceName), 
+Device::Device(TwoWire* wire, uint8_t i2cAddress, uint8_t tcaChannel, const String& deviceName, int deviceIndex)
+    : wire(wire), i2cAddress(i2cAddress), tcaChannel(tcaChannel), deviceName(deviceName), 
       deviceIndex(deviceIndex), type("Generic"), typeNumber(""), initialized(false), threshold(0.0f) {
 }
 
 // Constructor for devices that specify threshold and channels first
-Device::Device(float threshold, std::map<String, String> channels, uint8_t i2cAddress, uint8_t tcaChannel, int deviceIndex)
-    : i2cAddress(i2cAddress), tcaChannel(tcaChannel), deviceName(""), 
+Device::Device(TwoWire* wire, float threshold, std::map<String, String> channels, uint8_t i2cAddress, uint8_t tcaChannel, int deviceIndex)
+    : wire(wire), i2cAddress(i2cAddress), tcaChannel(tcaChannel), deviceName(""), 
       deviceIndex(deviceIndex), type("Generic"), typeNumber(""), initialized(false), 
       threshold(threshold), channels(channels) {
     // This constructor is used by devices that specify threshold and channels first
