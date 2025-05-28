ca5da54353ea3255be4d8754b585de05ccb1a0ea
GeR0Nim0O <88638730+GeR0Nim0O@users.noreply.github.com>
2025-05-28 21:26:31 +0200

    Auto-commit on save

--- lib/Sensors/SHTsensor/SHTsensor.h
+++ lib/Sensors/SHTsensor/SHTsensor.h
@@ -10,7 +10,7 @@
 
 class SHTsensor : public Device {
 public:
-    SHTsensor(uint8_t i2cAddress, uint8_t tcaChannel, float threshold, std::map<String, String> channels, int deviceIndex);
+    SHTsensor(TwoWire* wire, uint8_t i2cAddress, uint8_t tcaChannel, float threshold, std::map<String, String> channels, int deviceIndex);
     
     bool begin() override;    bool isConnected() override;
     void update() override;    std::map<String, String> readData() override; // Return a map of sensor data
