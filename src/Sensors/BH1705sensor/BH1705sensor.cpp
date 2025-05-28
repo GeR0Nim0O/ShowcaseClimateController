a43d2f76ff16c6524f7714c85e1a7c5fe8f3796d
GeR0Nim0O <88638730+GeR0Nim0O@users.noreply.github.com>
2025-05-28 20:14:14 +0200

    Auto-commit on save

--- src/Sensors/BH1705sensor/BH1705sensor.cpp
+++ src/Sensors/BH1705sensor/BH1705sensor.cpp
@@ -65,14 +65,14 @@ bool BH1705sensor::writeCommand(uint8_t command) {
 }
 
 bool BH1705sensor::readBytes(uint8_t *data, uint8_t length) {
-    if (wire->requestFrom(_address, length) != length)
+    if (Wire.requestFrom(_address, length) != length)
     {
         return false;
     }
 
     for (uint8_t i = 0; i < length; i++)
     {
-        data[i] = wire->read();
+        data[i] = Wire.read();
     }
 
     return true;
