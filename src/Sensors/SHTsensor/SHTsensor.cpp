3b0f413459b7d4fb6af6861213d06ef39b0272db
GeR0Nim0O <88638730+GeR0Nim0O@users.noreply.github.com>
2025-05-28 21:27:30 +0200

    Auto-commit on save

--- src/Sensors/SHTsensor/SHTsensor.cpp
+++ src/Sensors/SHTsensor/SHTsensor.cpp
@@ -246,11 +246,9 @@ bool SHTsensor::readBytes(uint8_t *data, uint8_t length)
     if (wire->requestFrom(_address, length) != length)
     {
         return false;
-    }
-
-    for (uint8_t i = 0; i < length; i++)
+    }    for (uint8_t i = 0; i < length; i++)
     {
-        data[i] = Wire.read();
+        data[i] = wire->read();
     }
 
     return true;
