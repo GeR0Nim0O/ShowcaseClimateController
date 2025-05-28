05a68cc8f0cefe7aacb0892c3ccfc0df5fe79d03
GeR0Nim0O <88638730+GeR0Nim0O@users.noreply.github.com>
2025-05-28 16:08:08 +0200

    Auto-commit on save

--- src/Sensors/SHTsensor/SHTsensor.cpp
+++ src/Sensors/SHTsensor/SHTsensor.cpp
@@ -236,10 +236,10 @@ bool SHTsensor::setHeater(bool enable)
 
 bool SHTsensor::writeCommand(uint16_t command)
 {
-    wire->beginTransmission(_address);
-    wire->write(command >> 8); // Send MSB
-    wire->write(command & 0xFF); // Send LSB
-    return wire->endTransmission() == 0;
+    Wire.beginTransmission(_address);
+    Wire.write(command >> 8); // Send MSB
+    Wire.write(command & 0xFF); // Send LSB
+    return Wire.endTransmission() == 0;
 }
 
 bool SHTsensor::readBytes(uint8_t *data, uint8_t length)
