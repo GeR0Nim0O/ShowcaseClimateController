63470d3ea0a65ec7813815a091aabe6918a71cbb
GeR0Nim0O <88638730+GeR0Nim0O@users.noreply.github.com>
2025-05-28 21:27:39 +0200

    Auto-commit on save

--- src/Sensors/SHTsensor/SHTsensor.cpp
+++ src/Sensors/SHTsensor/SHTsensor.cpp
@@ -265,8 +265,8 @@ float SHTsensor::getHumidity() const {
 // Implementation of pure virtual methods from Device base class
 bool SHTsensor::isConnected() {
     I2CHandler::selectTCA(getTCAChannel());
-    Wire.beginTransmission(_address);
-    return (Wire.endTransmission() == 0);
+    wire->beginTransmission(_address);
+    return (wire->endTransmission() == 0);
 }
 
 void SHTsensor::update() {
