60aba5b9c7b84d435cee69465934d5ba9e3b1045
GeR0Nim0O <88638730+GeR0Nim0O@users.noreply.github.com>
2025-05-28 20:14:17 +0200

    Auto-commit on save

--- lib/RTC/DS3231rtc/DS3231rtc.h
+++ lib/RTC/DS3231rtc/DS3231rtc.h
@@ -10,8 +10,8 @@
 
 class DS3231rtc : public Device {
 public:
-    DS3231rtc(TwoWire* wire, uint8_t i2cChannel, uint8_t tcaPort, float threshold, std::map<String, String> channels, int deviceIndex)
-        : Device(threshold, channels, i2cChannel, tcaPort, deviceIndex), wire(wire) {}
+    DS3231rtc(uint8_t i2cChannel, uint8_t tcaPort, float threshold, std::map<String, String> channels, int deviceIndex)
+        : Device(threshold, channels, i2cChannel, tcaPort, deviceIndex) {}
     
     bool begin() override {
         wire->begin();
@@ -32,7 +32,6 @@ public:
                 unsigned char year);
     
 private:
-    TwoWire* wire;
     unsigned char decToBcd(unsigned char val);
     unsigned char bcdToDec(unsigned char val);
 };
