5a73321e00f899e5c8108eb37874ccb162a6dba2
GeR0Nim0O <88638730+GeR0Nim0O@users.noreply.github.com>
2025-05-28 10:51:29 +0200

    Auto-commit on save

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,7 @@ PubSubClient client(espClient);
 
 // Custom WiFi settings
 String customSSID = "Bruns-Guest";
-String customPassword = "Gast@Ron&Rowie";
+String customPassword = "Guest@bruns";
 bool useCustomWiFi = true; // Set to true to use custom WiFi settings
 
 // Custom MQTT settings
