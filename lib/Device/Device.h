b096c957d6d1d5a92eac9d7811da1d694c461eb8
GeR0Nim0O <88638730+GeR0Nim0O@users.noreply.github.com>
2025-05-28 21:23:24 +0200

    Auto-commit on save

--- lib/Device/Device.h
+++ lib/Device/Device.h
@@ -8,8 +8,8 @@
 
 class Device {
 public:
-    Device(uint8_t i2cAddress, uint8_t tcaChannel, const String& deviceName, int deviceIndex);
-    Device(float threshold, std::map<String, String> channels, uint8_t i2cAddress, uint8_t tcaChannel, int deviceIndex);
+    Device(TwoWire* wire, uint8_t i2cAddress, uint8_t tcaChannel, const String& deviceName, int deviceIndex);
+    Device(TwoWire* wire, float threshold, std::map<String, String> channels, uint8_t i2cAddress, uint8_t tcaChannel, int deviceIndex);
     virtual ~Device() = default;
     
     virtual bool begin() = 0;
