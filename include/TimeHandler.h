3f167201281b6699479bd095b81e75c6cf751449
GeR0Nim0O <88638730+GeR0Nim0O@users.noreply.github.com>
2025-05-30 20:59:16 +0200

    Auto-commit on save

--- include/TimeHandler.h
+++ include/TimeHandler.h
@@ -11,11 +11,12 @@ public:
     static void setRTCFromAPI(DS3231rtc& rtc, String datetime); // Set RTC time from API
     static String getTimeFromRTC(DS3231rtc& rtc); // Get time from RTC
     static void updateRTCFromNTP(DS3231rtc& rtc); // Update RTC from NTP server
-    static String getCurrentTime(DS3231rtc& rtc); // Get current time
-
-    // New function to fetch current time periodically
+    static String getCurrentTime(DS3231rtc& rtc); // Get current time    // New function to fetch current time periodically
     static void fetchCurrentTimePeriodically(DS3231rtc* rtc, unsigned long& lastTimeFetch, const unsigned long timeFetchInterval);
     static String fetchTime(DS3231rtc& rtc); // Fetch time from API or RTC
+    
+    // Status reporting
+    static void printTimeStatus(DS3231rtc* rtc);
 
 private:
     static String fetchTimeFromAPI(const String& url); // Fetch time from the given API URL
