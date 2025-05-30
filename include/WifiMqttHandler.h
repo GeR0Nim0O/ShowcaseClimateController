b73721b6ba076386b007abce8027391a56cd819c
GeR0Nim0O <88638730+GeR0Nim0O@users.noreply.github.com>
2025-05-30 20:58:38 +0200

    Auto-commit on save

--- include/WifiMqttHandler.h
+++ include/WifiMqttHandler.h
@@ -12,12 +12,13 @@ public:
     static void keepAlive(PubSubClient &client, WiFiClientSecure &espClient, const char* ssid, const char* password, const char* mqtt_server, const char* rootCACertificate, int mqtt_port, const char* clientId, const char* topic);
 
     // New function to connect to WiFi with check
-    static bool connectToWiFiWithCheck(const String& ssid, const String& password);
-
-    // New function to connect to MQTT broker with check
+    static bool connectToWiFiWithCheck(const String& ssid, const String& password);    // New function to connect to MQTT broker with check
     static bool connectToMqttBrokerWithCheck(PubSubClient &client, WiFiClientSecure &espClient, const String& mqtt_server, const char* rootCACertificate, int mqtt_port, const String& clientId, const String& topic, const String& token = "");
 
     static void setupSecureClient(WiFiClientSecure &espClient, const char* rootCACertificate);
+    
+    // Status reporting functions
+    static void printConnectionStatus(PubSubClient &client);
 };
 
 #endif // WIFIMQTTHANDLER_H
