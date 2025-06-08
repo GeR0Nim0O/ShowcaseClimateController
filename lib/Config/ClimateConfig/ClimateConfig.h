94a850ec29bd1661d3f0a70b6241f8b49021bbe3
GeR0Nim0O <88638730+GeR0Nim0O@users.noreply.github.com>
2025-06-08 16:16:26 +0200

    Auto-commit on save

--- lib/Config/ClimateConfig/ClimateConfig.h
+++ lib/Config/ClimateConfig/ClimateConfig.h
@@ -68,7 +68,8 @@ public:
     float getMaxTemperature() const { return settings.maxTemperature; }
     float getMinTemperature() const { return settings.minTemperature; }
     float getMaxHumidity() const { return settings.maxHumidity; }
-    float getMinHumidity() const { return settings.minHumidity; }    String getClimateMode() const { return String(settings.climateMode); }
+    float getMinHumidity() const { return settings.minHumidity; }
+    String getClimateMode() const { return String(settings.climateMode); }
     String getHumidityMode() const { return String(settings.humidityMode); }
     bool getAutoFanControl() const { return settings.autoFanControl; }    float getTemperatureHysteresis() const { return settings.temperatureHysteresis; }
     float getHumidityHysteresis() const { return settings.humidityHysteresis; }
