1fce05061f26ea88bac9e27414126d8e137df2f6
GeR0Nim0O <88638730+GeR0Nim0O@users.noreply.github.com>
2025-06-11 14:15:26 +0200

    Auto-commit on save

--- src/Sensors/RotaryEncoder/RotaryEncoder.cpp
+++ src/Sensors/RotaryEncoder/RotaryEncoder.cpp
@@ -122,14 +122,6 @@ void RotaryEncoder::update() {
 uint16_t RotaryEncoder::getEncoderValue() {
     selectTCAChannel(tcaChannel);
     uint16_t value = readRegister16(VISUAL_ROTARY_ENCODER_COUNT_MSB_REG);
-    
-    // Debug output occasionally
-    static int callCount = 0;
-    callCount++;
-    if (callCount % 100 == 0) {
-        Serial.printf("[RotaryEncoder] getEncoderValue() call #%d returned: %d\n", callCount, value);
-    }
-    
     return value;
 }
 
